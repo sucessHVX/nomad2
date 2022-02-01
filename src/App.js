8c9cd6b3e9e6e106cd30cf102091d7885e432d8e
Jaemin Ha <woals3527@naver.com>
2022-02-01 15:30:08 +0900

    sc commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function Btn({ text, onClick }) {
+    return <button onClick={onClick}>{text}</button>;
+  }
+  const [value, setValue] = useState('Save Changes');
+  const changeValue = () => {
+    value === 'Save Changes'
+      ? setValue('Revert Changes')
+      : setValue('Save Changes');
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Btn text={value} onClick={changeValue} />
+      <Btn text="Continue" />
     </div>
   );
 }
